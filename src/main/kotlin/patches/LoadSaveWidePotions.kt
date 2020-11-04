2980c3f6250088d0cc9241f589e539670561af65
Kiooeht <kiooeht@gmail.com>
2020-11-03 19:47:41 -0800

    Loading saves with wide potions

---
+++ src/main/kotlin/patches/LoadSaveWidePotions.kt
@@ -0,0 +1,28 @@
+package com.evacipated.cardcrawl.mod.widepotions.patches
+
+import com.evacipated.cardcrawl.mod.widepotions.potions.WidePotion
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn
+import com.megacrit.cardcrawl.helpers.PotionHelper
+import com.megacrit.cardcrawl.potions.AbstractPotion
+
+@SpirePatch(
+    clz = PotionHelper::class,
+    method = "getPotion"
+)
+object LoadSaveWidePotions {
+    @JvmStatic
+    fun Prefix(name: String?): SpireReturn<AbstractPotion?> {
+        if (name != null) {
+            if (name.startsWith("wide:")) {
+                val realPotion = PotionHelper.getPotion(name.removePrefix("wide:"))
+                if (realPotion != null) {
+                    return SpireReturn.Return(WidePotion(realPotion))
+                }
+            } else if (name.startsWith("wideright:")) {
+                return SpireReturn.Return(null)
+            }
+        }
+        return SpireReturn.Continue()
+    }
+}
