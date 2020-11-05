04a510cd4186ce5c83f9aacfa7425fa457d0b29a
Kiooeht <kiooeht@gmail.com>
2020-11-04 20:18:45 -0800

    Custom Wide Blessing of the Forge

--- src/main/kotlin/potions/WidePotion.kt
+++ src/main/kotlin/potions/WidePotion.kt
@@ -110,7 +110,8 @@ class WidePotion(
 
         val whitemap = mapOf<String, () -> AbstractPotion>(
             WeakenPotion.POTION_ID to ::WideWeakenPotion,
-            FearPotion.POTION_ID to ::WideFearPotion
+            FearPotion.POTION_ID to ::WideFearPotion,
+            BlessingOfTheForge.POTION_ID to ::WideBlessingOfTheForge
         )
     }
 }
