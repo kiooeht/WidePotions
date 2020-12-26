e3a3bde91e965ee8256cdbbb6f8011b129ed824d
Kiooeht <kiooeht@gmail.com>
2020-12-26 15:13:41 -0800

    Simplify wide fear potion

--- src/main/kotlin/potions/WideFearPotion.kt
+++ src/main/kotlin/potions/WideFearPotion.kt
@@ -1,34 +1,14 @@
 package com.evacipated.cardcrawl.mod.widepotions.potions
 
-import com.evacipated.cardcrawl.mod.widepotions.WidePotionsMod
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
 import com.megacrit.cardcrawl.core.AbstractCreature
-import com.megacrit.cardcrawl.core.CardCrawlGame
-import com.megacrit.cardcrawl.core.Settings
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
-import com.megacrit.cardcrawl.potions.AbstractPotion
 import com.megacrit.cardcrawl.potions.FearPotion
 import com.megacrit.cardcrawl.powers.VulnerablePower
-import com.megacrit.cardcrawl.ui.panels.TopPanel
 
-class WideFearPotion : FearPotion(), IsWidePotion {
+class WideFearPotion : WidePotion(FearPotion()) {
     init {
-        val strings = CardCrawlGame.languagePack.getPotionString(WidePotionsMod.makeID("WidePotion"))
-        name = strings.NAME.format(name)
-
-        isThrown = true
         targetRequired = false
-
-        initializeData()
-
-        hb.resize(hb.width * 2, hb.height)
-    }
-
-    override fun initializeData() {
-        super.initializeData()
-
-        description = strings.DESCRIPTIONS[0].format(potency)
-        tips[0].body = description
     }
 
     override fun use(target: AbstractCreature?) {
@@ -40,25 +20,6 @@ class WideFearPotion : FearPotion(), IsWidePotion {
     }
 
     override fun getPotency(ascensionLevel: Int): Int {
-        return super.getPotency(ascensionLevel) * 2
-    }
-
-    override fun makeCopy(): AbstractPotion =
-        WideFearPotion()
-
-    override fun setAsObtained(potionSlot: Int) {
-        super.setAsObtained(potionSlot)
-
-        AbstractDungeon.player.obtainPotion(potionSlot + 1, WidePotionRightHalf(this))
-    }
-
-    override fun adjustPosition(slot: Int) {
-        super.adjustPosition(slot)
-
-        hb.move(TopPanel.potionX + (slot + 0.5f) * Settings.POTION_W, posY)
-    }
-
-    companion object {
-        private val strings = CardCrawlGame.languagePack.getPotionString(WidePotionsMod.makeID(POTION_ID))
+        return potion.getPotency(ascensionLevel) * 2
     }
 }
