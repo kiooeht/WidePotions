f25695aed4603d6dba54b50974a54b154a22bc1f
Kiooeht <kiooeht@gmail.com>
2020-12-26 17:43:21 -0800

    Fix wide potion belt hitbox moving when obtaining another relic

--- src/main/kotlin/relics/WidePotionBelt.kt
+++ src/main/kotlin/relics/WidePotionBelt.kt
@@ -6,6 +6,7 @@ import com.evacipated.cardcrawl.mod.widepotions.extensions.setPrivateFinal
 import com.evacipated.cardcrawl.mod.widepotions.potions.IsWidePotion
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.helpers.Hitbox
 import com.megacrit.cardcrawl.helpers.PowerTip
 import com.megacrit.cardcrawl.potions.PotionSlot
 import com.megacrit.cardcrawl.relics.AbstractRelic
@@ -22,6 +23,9 @@ class WidePotionBelt : PotionBelt(), IsWidePotion {
         description = strings.DESCRIPTIONS[0].format(EXTRA_SLOTS)
         tips[0] = PowerTip(name, description)
         privateMethod("initializeTips", clazz = AbstractRelic::class).invoke<Unit>()
+
+        hb = MyHitbox(hb)
+        hb.resize(PAD_X * 2, PAD_X)
     }
 
     override fun onEquip() {
@@ -32,7 +36,10 @@ class WidePotionBelt : PotionBelt(), IsWidePotion {
     }
 
     override fun update() {
-        hb.resize(PAD_X * 2, PAD_X)
+        if (hb !is MyHitbox) {
+            hb = MyHitbox(hb)
+            hb.resize(PAD_X * 2, PAD_X)
+        }
         super.update()
     }
 
@@ -41,5 +48,15 @@ class WidePotionBelt : PotionBelt(), IsWidePotion {
     companion object {
         private val strings by lazy { CardCrawlGame.languagePack.getRelicStrings(WidePotionsMod.makeID(ID)) }
         private const val EXTRA_SLOTS = 4
+    }
+
+    private class MyHitbox : Hitbox {
+        constructor(hb: Hitbox) : this(hb.x, hb.y, hb.width, hb.height)
+        constructor(width: Float, height: Float) : super(width, height)
+        constructor(x: Float, y: Float, width: Float, height: Float) : super(x, y, width, height)
+
+        override fun move(cX: Float, cY: Float) {
+            super.move(cX + AbstractRelic.PAD_X / 2f, cY)
+        }
     }
 }
