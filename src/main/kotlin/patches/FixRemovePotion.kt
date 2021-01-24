c3f0a62dea4475e666711f240ac1558acda96d9f
Kiooeht <kiooeht@gmail.com>
2021-01-23 17:10:35 -0800

    Fix removing wide potion from player

---
+++ src/main/kotlin/patches/FixRemovePotion.kt
@@ -0,0 +1,23 @@
+package com.evacipated.cardcrawl.mod.widepotions.patches
+
+import com.evacipated.cardcrawl.mod.widepotions.extensions.isWide
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.potions.AbstractPotion
+import com.megacrit.cardcrawl.potions.PotionSlot
+
+@SpirePatch(
+    clz = AbstractPlayer::class,
+    method = "removePotion"
+)
+object FixRemovePotion {
+    @JvmStatic
+    fun Prefix(__instance: AbstractPlayer, potionOption: AbstractPotion) {
+        if (potionOption.isWide()) {
+            val slot = __instance.potions.indexOf(potionOption)
+            if (slot >= 0) {
+                __instance.potions[slot + 1] = PotionSlot(slot + 1)
+            }
+        }
+    }
+}
