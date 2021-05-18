529c996652f6e0ce34f48b34e249486bdff99b29
Kiooeht <kiooeht@gmail.com>
2021-05-17 18:42:42 -0700

    Fix removePotion unable to remove wide potion slots

--- src/main/kotlin/patches/FixRemovePotion.kt
+++ src/main/kotlin/patches/FixRemovePotion.kt
@@ -1,6 +1,8 @@
 package com.evacipated.cardcrawl.mod.widepotions.patches
 
 import com.evacipated.cardcrawl.mod.widepotions.extensions.isWide
+import com.evacipated.cardcrawl.mod.widepotions.extensions.widepotions
+import com.evacipated.cardcrawl.mod.widepotions.potions.WidePotionSlot
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.potions.AbstractPotion
@@ -17,6 +19,11 @@ object FixRemovePotion {
             val slot = __instance.potions.indexOf(potionOption)
             if (slot >= 0) {
                 __instance.potions[slot + 1] = PotionSlot(slot + 1)
+            } else {
+                val wideSlot = __instance.widepotions.indexOf(potionOption)
+                if (wideSlot >= 0) {
+                    __instance.widepotions[wideSlot] = WidePotionSlot(potionOption.slot)
+                }
             }
         }
     }
